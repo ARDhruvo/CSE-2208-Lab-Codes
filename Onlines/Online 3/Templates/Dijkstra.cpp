056920fea0aaf5113f3b7727cf6d9668c4a4ee10
Kashikizu <aribur.cse.20220204003@aust.edu>
2025-01-10 13:33:51 +0600

    Online 3

--- Onlines/Online 3/Templates/Dijkstra.cpp
+++ Onlines/Online 3/Templates/Dijkstra.cpp
@@ -2,48 +2,59 @@
 using namespace std;
 
 #define paragraph cout << endl
+#define INF 255
+#define NIL -1
+#define is ==
+#define isnt !=
 
-/*
-For Acyclic
-5 10
-1 2 10
-1 3 5
-2 4 1
-2 3 2
-3 2 3
-3 4 9
-3 5 2
-4 5 4
-5 4 6
-5 1 7
+int node_no, edge_no;
+vector<int> nodes;
 
-5 10
-5 1 7
-5 4 6
-2 4 1
-4 5 4
-3 2 3
-3 4 9
-3 5 2
-2 3 2
-1 3 5
-1 2 10
+void adjMatrix(vector<vector<int>> W) // Printing Function for the Cost Matrix
+{
 
- */
+    cout << "Adjacency Cost Matrix: " << endl
+         << endl
+         << "\t";
+    for (int i = 1; i < node_no; i++)
+    {
+        cout << i << "    "; // Prints out columns
+    }
+    paragraph;
 
-int node_no, edge_no;       // Number of edges and nodes
-vector<int> Node_Vec[100];  // Main Graph
-vector<int> Node_Cost[100]; // Graph Weight/Cost
-vector<int> nodes;          // d is min cost and pre is previous node
-int d[100], pre[100];
-
+    for (int i = 1; i < node_no; i++)
+    {
+        cout << i << "\t";
+        for (int j = 1; j < node_no; j++) // Prints out rows
+        {
+            if (W[i][j] isnt INF) // Checks if there is connection with j-th node
+            {
+                cout << W[i][j];
+                if ((W[i][j] >= 0) && (W[i][j] < 10)) // Checks negative for spacing reasons
+                {
+                    cout << "    ";
+                }
+                else
+                {
+                    cout << "   "; // Negative numbers get one less space because I have OCD
+                }
+            }
+            else
+            {
+                cout << "INF  "; // 255 is definied as INF it will not actually print out INF
+            }
+        }
+        paragraph;
+    }
+    paragraph;
+}
 void insNode(int node)
 {
     if (!nodes.empty())
     {
         for (int u : nodes)
         {
-            if (u == node)
+            if (u is node)
             {
                 return;
             }
@@ -56,13 +67,13 @@ void insNode(int node)
     }
 }
 
-void pathPrint(int src)
+void pathPrint(vector<int> d, vector<int> pre, int src)
 {
     // Print
     for (int n : nodes)
     {
         cout << n << ": ";
-        if (d[n] == 255) // Inf means its not possible
+        if (d[n] is INF) // Inf means its not possible
         {
             cout << "Not possible" << endl;
         }
@@ -71,7 +82,7 @@ void pathPrint(int src)
             cout << src;
             stack<int> path;
             int x = n;
-            while (n != src) // Pushes the nodes into the stack until source is found
+            while (n isnt src) // Pushes the nodes into the stack until source is found
             {
                 path.push(n);
                 n = pre[n];
@@ -86,144 +97,92 @@ void pathPrint(int src)
     }
 }
 
-void initializeSingleSource(int src)
+void dijkstra(vector<vector<int>> graph, vector<vector<int>> cost, int src)
 {
-    // Initialize Single Source
+    vector<int> d(node_no); // d[i] is min cost to reach node i from source
+    vector<int> pi(node_no); // pi[i] is immediate predecessor node to reach node i
 
-    // cout << "reached initializing for " << src << endl;
+    // Initialize Single Source:
     for (int i : nodes)
     {
-         // cout << i << " ";
-        d[i] = 255; // 255 = inf
-        pre[i] = 0; // 0 = Nil
+        d[i] = INF; // Sets all the initial cost to reach as INF
+        pi[i] = NIL; // Sets all the initial predecessor to NIL
     }
-    paragraph;
-    d[src] = 0; // Sets source to 0 as the cost will always be 0
-    paragraph;
-}
+    d[src] = 0; // Sets the cost to reach the source to 0 as it will always be 0 to reach your starting position
 
-void relax(int u)
-{
-    // Relax
-    for (int i = 0; i < Node_Vec[u].size(); i++)
+    set<int> S; // Set for checking visited nodes
+    priority_queue<pair<int, int>> Q; // Takes a priority Queue which takes in pair of (cost to reach node i) and (i)
+    // This will help to sort and find the lightest edge from the currently exploring node
+    Q.push(make_pair(d[src], src)); // Pushes the (min cost to reach source, source) since the first step starts from source 
+
+    while (!Q.empty()) // Q will be empty once all the nodes have been explored
     {
-        int v = Node_Vec[u][i];              // v is adjacent node
-        cout << u << " " << v << endl << d[u] << " " << Node_Cost[u][i] << " " << d[v] << endl;
-        if (d[v] > (d[u] + Node_Cost[u][i])) // checks if the new cost is smaller
-        {
-
-            d[v] = d[u] + Node_Cost[u][i]; // Updates cost if smaller
-            pre[v] = u;                    // Sets previous node as the currently cycling one
-        }
-
-    }
-    paragraph;
-}
-
-void dijkstra(int src)
-{
-    initializeSingleSource(src);
-    vector<int> S;
-    priority_queue<int> Q;
-    for (int u : nodes)
-    {
-
-        Q.push(-u);
-    }
-    while(!Q.empty())
-    {
-        int u = -Q.top();
+        int u = Q.top().second; // The second element of the pair is the node to be explored
         Q.pop();
-        // cout << u << endl;
-        relax(u);
+
+        if (S.find(u) != S.end()) // If node has been visited it will skip
+        {
+            continue;
+        }
+        S.insert(u); // Else, insert the node in the visited set
+
+        // Relax
+        for (int i = 0; i < graph[u].size(); i++) // For all adjacent nodes of u
+        {
+            int v = graph[u][i]; // v is the adjacent node of u which updates every iteration
+            if (d[v] > (d[u] + cost[u][v])) // If the cost of using source and present node is lower than the previous min cost then it will be updated
+            {
+                d[v] = (d[u] + cost[u][v]);
+                pi[v] = u; // Updates the predecessor to be the currently exploring node
+                Q.push(make_pair(-d[v], v)); // Pushes the negative value of the edge with the adjacent node to explore the adjacent node with the lowest value next iteration
+                // Negative Value is pushed as priority Queue uses max heap instead of min heap
+            }
+        }
     }
-    pathPrint(src);
+    paragraph;
+
+    pathPrint(d, pi, src);
 }
-
-// void DFS(int u)
-// {
-//     color[u] = GRAY;
-//     for (int i = 0; i < Node_Vec[u].size(); i++)
-//     {
-//         int v = Node_Vec[u][i];
-//         if (color[v] == GRAY) // To check if cycle
-//         {
-//             cycle = true;
-//             return;
-//         }
-//         else if (color[v] == WHITE)
-//         {
-//             DFS(v);
-//         }
-//     }
-//     color[u] = BLACK;
-//     TopS.insert(TopS.begin(), u); // In order to sort topologically, the processed node is to be inserted at first
-// }
-
-// void shortestDAG(int src)
-// {
-//     // Topological Sort
-//     for (int i = 1; i <= node_no; i++) // Runs DFS
-//     {
-//         if (color[i] == WHITE)
-//         {
-//             DFS(i);
-//         }
-//     }
-//     if (cycle) // Checks if cycle is present
-//     {
-//         cout << "Cycle Found" << endl;
-//         return;
-//     }
-//     cout << "Topologically Sorted: " << endl;
-//     for (int i : TopS)
-//     {
-//         cout << i << " ";
-//     }
-//     paragraph;
-
-//     // Cost Update
-
-// }
 
 int main()
 {
     cout << "Enter Number of Nodes and Edges: ";
     cin >> node_no >> edge_no;
+    node_no++; // Increased for indexing the vector
     paragraph;
 
-    cout << "Enter connections and cost:" << endl;
-    for (int i = 0; i < edge_no; i++)
+    vector<vector<int>> Cost(node_no, vector<int>(node_no, INF)); // The cost matrix is initialized with INF; Assumed at start that all nodes are disconnected
+    // This is done for disconnected graphs
+    vector<vector<int>> Adj(node_no); // The Adjacent Matrix's i-th vector contains all adjacent nodes of i
+
+    cout << "Enter connections and cost: " << endl;
+    int nodeA, nodeB, cost;
+    for (int i = 1; i <= edge_no; i++)
     {
-        int nodeA, nodeB, cost;
         cin >> nodeA >> nodeB >> cost;
-        insNode(nodeA);
-        insNode(nodeB);
-        Node_Vec[nodeA].push_back(nodeB); // To make it directed
-        Node_Cost[nodeA].push_back(cost);
-    }
-    paragraph;
-    cout << "Adjacency List:" << endl;
-    for (int i : nodes)
-    {
-        cout << i << " -> ";
-        for (int j = 0; j < Node_Vec[i].size(); j++)
+        if (cost < 0)
         {
-            cout << Node_Vec[i][j] << " (" << Node_Cost[i][j] << "), "; // Node -> Adjacent (Cost)
+            cout << "Negative Edge Not Allowed.\nPlease Enter Again" << endl;
+            i--; // Resets the iteration number by 1 if negative value is entered
+            paragraph;
         }
-        paragraph;
+        else
+        {
+            Cost[nodeA][nodeB] = cost;   // Sets the [i][j]-th element of the matrix to be the cost to visit j from i
+            Adj[nodeA].push_back(nodeB); // Updates the Adjacent Matrix
+            // Keeps track of all nodes inserted in the graph
+            insNode(nodeA);
+            insNode(nodeB);
+        }
     }
-    paragraph;
-    // for (int i : nodes)
-    // {
-    //     cout << i << " ";
-    // }
     paragraph;
 
-    cout << "Input the source for dijkstra: ";
+    adjMatrix(Cost); // Prints the Adjacency Matrix
+
     int src;
+    cout << "Enter source for Dijkstra: ";
     cin >> src;
     paragraph;
 
-    dijkstra(src);
+    dijkstra(Adj, Cost, src);
 }
