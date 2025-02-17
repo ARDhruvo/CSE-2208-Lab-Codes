b4b318d76ff426ebe3148caae03d423b6256d02d
ARDhruvo <aribur.cse.20220204003@aust.edu>
2025-02-17 08:48:51 +0600

    TSP Update 002: Untested Skeleton

--- Offlines/Offline 4 - Travelling Salesman Problem/tsp.cpp
+++ Offlines/Offline 4 - Travelling Salesman Problem/tsp.cpp
@@ -6,7 +6,10 @@ using namespace std;
 #define vertex pair<int, pair<int, vector<vector<int>>>>
 #define minheap vertex, vector<vertex>, greater<vertex>
 
+int node_no, edge_no;
+
 vector<int> path;
+int finalCost;
 
 void AdjMatrix(set<int> Nodes, vector<vector<int>> W)
 {
@@ -37,13 +40,62 @@ void AdjMatrix(set<int> Nodes, vector<vector<int>> W)
     }
 }
 
-void tsp(int src, set<int> Nodes, vector<vector<int>> Cost)
+int reductionCost(vector<vector<int>> &Cost)
 {
-    path.push_back(src);
-    priority_queue<minheap> Q;
+    int cost = 0;
 
+    for (int i = 1; i < node_no; i++)
+    {
+        int red = INF;
+        for (int j = 1; j < node_no; j++)
+        {
+            if (Cost[i][j] < red)
+            {
+                red = Cost[i][j];
+            }
+        }
+        if (red != INF)
+        {
+
+            for (int j = 1; j < node_no; j++)
+            {
+                if (Cost[i][j] != INF)
+                {
+                    Cost[i][j] -= red;
+                }
+            }
+            cost += red;
+        }
+    }
+
+    for (int j = 1; j < node_no; j++)
+    {
+        int red = INF;
+        for (int i = 1; i < node_no; i++)
+        {
+            if (Cost[i][j] < red)
+            {
+                red = Cost[i][j];
+            }
+        }
+        if (red != INF)
+        {
+
+            for (int i = 1; i < node_no; i++)
+            {
+                if (Cost[i][j] != INF)
+                {
+                    Cost[i][j] -= red;
+                }
+            }
+            cost += red;
+        }
+    }
+}
+
+void recurtsp(int src, set<int> Nodes, vector<vector<int>> Cost)
+{
     /*
-     * Reduction
      * for each node v in Nodes:
      * * r = Cost[src][v]
      * * set [src] row Cost to INF
@@ -52,12 +104,57 @@ void tsp(int src, set<int> Nodes, vector<vector<int>> Cost)
      * * Cost = r + Reduction of current matrix
      * * Q.push -> {Cost: {v : Current Matrix}}
      */
+
+    if (!Nodes.empty())
+    {
+        Nodes.erase(src);
+        path.push_back(src);
+        priority_queue<minheap> Q;
+        vector<vector<int>> Dummy(Nodes.size(), vector<int>(node_no, 0));
+        int dummIndex = 0;
+
+        for (int v : Nodes)
+        {
+            int r = Cost[src][v];
+            if (r == INF)
+            {
+                continue;
+            }
+            Dummy = Cost;
+            for (int i = 0; i < node_no; i++)
+            {
+                Dummy[src][i] = INF;
+                Dummy[i][v] = INF;
+            }
+            Dummy[src][v] = INF;
+            Dummy[v][src] = INF;
+            int cost = finalCost + reductionCost(Dummy) + r;
+            Q.push({cost, {v, Dummy}});
+        }
+
+        Dummy = Q.top().second.second;
+        finalCost = Q.top().second.first;
+        int nextSrc = Q.top().first;
+        Q.pop();
+
+        recurtsp(nextSrc, Nodes, Dummy);
+
+        /*
+         * Compare finalCost with the new Q.top()
+         * if finalCost is less: win
+         * else: I have no idea lol
+         */
+    }
+}
+
+void tsp(int src, set<int> Nodes, vector<vector<int>> Cost)
+{
+    finalCost = reductionCost(Cost);
+    recurtsp(src, Nodes, Cost);
 }
 
 int main()
 {
-    int node_no, edge_no;
-
     cout << "Enter Number of Nodes and Number of Edges: ";
     cin >> node_no >> edge_no;
     node_no++; // As 0 is not counted as a node
@@ -93,7 +190,6 @@ int main()
     int src;
     cout << "Enter source for TSP: ";
     cin >> src;
-    Nodes.erase(src);
     tsp(src, Nodes, Cost);
     paragraph;
 }
